4e21fc24c04694a3ee1308d6184101f2fe6620ed
Abiola <66026349+Webui-lab@users.noreply.github.com>
2023-08-30 00:46:29 +0100

    skills

---
+++ my-portfolio/src/about/PersonalInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PersonalInfo = () => {
+  return (
+    <div>My name is Omotosho Kamalideen Opeyemi, i am a frontend developer with strong interest in creating eye-catchy designs and writing readable and clean codes</div>
+  )
+}
+
+export default PersonalInfo
