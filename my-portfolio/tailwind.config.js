a4b7aa10b7f2d7a3c04587f61e41f14cf6dda174
Abiola <66026349+Webui-lab@users.noreply.github.com>
2023-08-30 04:52:11 +0100

    setup

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        md: { min: "416px", max: "850px" },
+
+        sm: { max: "415px" },
+      },
+    },
   },
   plugins: [],
 };
