2161e5ce9bbbf6a1115a4bfbf877503e02ca372c
Abiola <66026349+Webui-lab@users.noreply.github.com>
2023-09-01 11:57:39 +0100

    layout

---
+++ my-portfolio/src/works/Works.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Works = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img />
+        </div>
+        <p></p>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Works;
