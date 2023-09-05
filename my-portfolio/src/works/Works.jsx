d9947f13f133b550a5cad2d19060a80fcff81d9c
Abiola <dapious99@gmail.com>
2023-09-06 00:49:46 +0100

    image

--- my-portfolio/src/works/Works.jsx
+++ my-portfolio/src/works/Works.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import Button from "../button/Button";
+import workOne from "../assets/codevanta.jpg";
 
 const Works = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <img />
+    <div className="bg-slate-950 p-4 h-screen text-white">
+      <div className="flex gap-4 bg-black shadow-xl rounded-xl p-4">
+        <div className="w-[45rem]">
+          <img src={workOne} className="rounded-lg" />
         </div>
-        <p></p>
         <div>
-          <a href="https://codevanta.tech">
-            <Button
-              text={"View Site"}
-              style="bg-white border-2 border-blue-700 rounded-full px-3 py-2"
-            />
-          </a>
-          <a href="https://github.com/Code-Vanta/CompanyWebsite.git">
-            <Button
-              text={"View Code"}
-              style="bg-white border-2 border-blue-700 rounded-full px-3 py-2"
-            />
-          </a>
+          <p>CodeVanta Website</p>
+          <p>Codevanta is a young start-up</p>
+          <ul className="flex gap-2">
+            <li>ReactJs</li>
+            <li>TailwindCSS</li>
+            <li>Vercel</li>
+          </ul>
+          <div>
+            <a href="https://codevanta.tech">
+              <Button
+                text={"View Site"}
+                style="bg-black border-2 border-white rounded-full px-6 py-3"
+              />
+            </a>
+            <a href="https://github.com/Code-Vanta/CompanyWebsite.git">
+              <Button
+                text={"View Code"}
+                style="bg-black border-2 border-white rounded-full px-6 py-3"
+              />
+            </a>
+          </div>
         </div>
       </div>
       <div></div>
