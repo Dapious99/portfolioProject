20174f965fbfad140c63c846d2a43a738cab29bb
Abiola <dapious99@gmail.com>
2023-09-07 04:11:56 +0100

    codevanta writeup

--- my-portfolio/src/works/Works.jsx
+++ my-portfolio/src/works/Works.jsx
@@ -4,20 +4,27 @@ import workOne from "../assets/codevanta.jpg";
 
 const Works = () => {
   return (
-    <div className="bg-slate-950 p-4 h-screen text-white">
+    <div className="bg-black p-4 h-screen text-white">
       <div className="flex gap-4 bg-black shadow-xl rounded-xl p-4">
         <div className="w-[45rem]">
           <img src={workOne} className="rounded-lg" />
         </div>
         <div>
           <p>CodeVanta Website</p>
-          <p>Codevanta is a young start-up</p>
+          <p>
+            Codevanta is a young start-up that tries to understand the need of
+            individuals/businesses technologically and providing a suitable
+            solution for their need through their cutting-edge solutions to
+            development and expert proffesionals. The aim of Codevanta is to
+            revolutionize the way people and businesses access and utilize
+            services.
+          </p>
           <ul className="flex gap-2">
             <li>ReactJs</li>
             <li>TailwindCSS</li>
             <li>Vercel</li>
           </ul>
-          <div>
+          <div className="flex gap-3">
             <a href="https://codevanta.tech">
               <Button
                 text={"View Site"}
