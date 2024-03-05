915257d0d971d5b7d595d5308f35507b5da377a5
Omotosho Kamalideen <dapious99@gmail.com>
2024-03-05 19:39:40 +0100

    reusable components adjustments

--- my-portfolio/src/homepage/Home.jsx
+++ my-portfolio/src/homepage/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "../button/Button";
+import Button from "../components/Button";
 import workS from "../assets/workspace.png";
 import { IoMdMail } from "react-icons/io";
 import { BiLogoLinkedin } from "react-icons/bi";
