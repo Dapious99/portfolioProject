ce3ec6e3c3b3122cf47575d7241a858728576e5e
Omotosho Kamalideen <dapious99@gmail.com>
2024-03-13 08:58:30 +0100

    wok on about page

--- my-portfolio/src/pages/About/components/Hobbies.jsx
+++ my-portfolio/src/pages/About/components/Hobbies.jsx
@@ -6,7 +6,7 @@ const Hobbies = () => {
       <h2 className="font-semibold text-[1.5rem] decoration-3 underline decoration-blue-700">
         Hobbies
       </h2>
-      <ul className="flex gap-3 sm:flex-wrap">
+      <ul className="flex gap-3 sm:flex-col xs:flex-col xs:gap-1 sm:gap-1">
         <li>Coding</li>
         <li>Reading</li>
         <li>Internet Surfing</li>
--- my-portfolio/src/pages/About/components/PersonalInfo.jsx
+++ my-portfolio/src/pages/About/components/PersonalInfo.jsx
@@ -6,15 +6,6 @@ const PersonalInfo = () => {
       <h2 className="font-semibold text-[1.5rem] decoration-3 underline decoration-blue-700">
         About Me
       </h2>
-      <p className="">
-        My name is Omotosho Kamalideen Opeyemi, I am a frontend developer with
-        strong interest in creating intuitive interfaces and writing readable
-        and clean codes. I am a capable and focused individual who can work
-        independently with little or no supervision and can work efficiently in
-        a team setup. I pay attention to details and do effectively replicate
-        and improve designs received from product designers to give the users a
-        good experience.
-      </p>{" "}
       <p>
         Hey there! I'm Omotosho Kamalideen Opeyemi, but you can just call me
         Kamal. I'm not just your run-of-the-mill frontend developer; I'm a
